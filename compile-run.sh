a8270883719c43d2c87ed29cfaa1d0296f42c6d9
Anya Vostinar <vostinar@carleton.edu>
2025-05-06 14:09:18 -0500

    updating run files and adding macros to web

--- compile-run.sh
+++ compile-run.sh
@@ -1,2 +1,2 @@
-g++ -O3 -DNDEBUG -msse4.2 -Wall -Wno-unused-function -std=c++17 -IEmpirical/include/ native.cpp -o project_native
-./project_native
+g++ -O3 -DNDEBUG -march=native -Wall -Wno-unused-function -std=c++17 -Isignalgp-lite/third-party/Empirical/include/ -Isignalgp-lite/include/ native.cpp -o native_project
+./native_project
